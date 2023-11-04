09e773b6596a12e293305ec2045d46368f077191
Pulkit <pgoel3379@gmail.com>
2023-11-04 08:26:31 +0530

    add basic terminal interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+
+mod terminal_inteface;
+
+use terminal_inteface::{print_grid, show_player_prompt}; 
+
+fn run_prompt(test_vec: &Vec<String>){
+    // show the play grid, 
+    // wait for user input, 
+    // update the specified position with the player's character 
+    // show the play grid, 
+    //repeat 
+
+    loop{
+        print_grid(test_vec); 
+
+        show_player_prompt(); 
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut test_vec = Vec::new(); 
+
+    for i in 1..10{
+        test_vec.push(i.to_string()); 
+    }
+
+    run_prompt(&test_vec); 
 }
---
+++ src/terminal_inteface.rs
@@ -0,0 +1,44 @@
+use std::io::Write;
+use std::io::stdin; 
+use std::io::stdout; 
+
+pub fn print_grid(grid_values: &Vec<String>){
+
+    if grid_values.len() != 9{
+        println!("no enough values to make the grid"); 
+        return; 
+    }
+
+    let mut grid_output_string = String::new(); 
+
+    for (i, value) in grid_values.iter().enumerate(){
+
+        grid_output_string.push_str(&format!("|{:^3}", value)); 
+
+        if i%3 == 2{
+            grid_output_string.push_str(&format!("|\n-------------\n"));
+        }
+
+    }
+
+    println!("{}", grid_output_string); 
+}
+
+
+pub fn show_player_prompt(){
+
+    let player_symbol = "x";
+
+    let mut stdout = stdout(); 
+    print!("player {}'s turn, pick a spot: ", player_symbol);
+
+    stdout.flush().expect("can't flush stdout: ");
+
+    let mut buffer = String::new(); 
+    stdin().read_line(&mut buffer).expect("can't read the argument"); 
+
+    let args: Vec<&str> = buffer.split_whitespace().collect(); 
+
+    println!("{:?}", args)
+
+}
